3df31541d543976e25eb2aefe04cd1021b871a7c
Johann Schopplich <mail@johannschopplich.com>
2023-07-05 19:11:07 +0200

    chore: Cacao Kit version without i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,7 @@ const shouldPrerender = process.env.NITRO_PRERENDER_PAGES === 'true'
 if (shouldPrerender) consola.info('Nitro page prerendering enabled')
 
 export default defineNuxtConfig({
-  // If you project doesn't require i18n, use the `without-i18n` branch instead:
-  // https://github.com/johannschopplich/cacao-kit-frontend/tree/chore/without-i18n
-  modules: ['@nuxtjs/i18n', '@unocss/nuxt', '@vueuse/nuxt', 'nuxt-kql'],
+  modules: ['@unocss/nuxt', '@vueuse/nuxt', 'nuxt-kql'],
 
   runtimeConfig: {
     public: {
@@ -24,39 +22,13 @@ export default defineNuxtConfig({
     },
   },
 
-  i18n: {
-    locales: [
-      {
-        code: 'en',
-        file: 'en.json',
-      },
-      {
-        code: 'de',
-        file: 'de.json',
-      },
-    ],
-    defaultLocale: 'en',
-    lazy: true,
-    langDir: 'locales',
-    strategy: 'prefix',
-    compilation: {
-      strictMessage: false,
-    },
-    bundle: {
-      fullInstall: false,
-      dropMessageCompiler: true,
-    },
-  },
-
   nitro: {
     prerender: {
       // Enable Nitro's crawler to prerender all pages (optional)
       // If Kirby content changes, the frontend will have to be rebuilt
       ...(shouldPrerender && {
         crawlLinks: shouldPrerender,
-        routes: ['/en'],
-        // Ignore homepage, which redirects to the user's locale
-        ignore: ['/'],
+        routes: ['/'],
       }),
     },
   },
--- plugins/site.ts
+++ plugins/site.ts
@@ -1,39 +1,16 @@
 import { siteQuery } from '~/queries'
 import type { FetchError } from 'ofetch'
-import type { Composer } from '#i18n'
 
-export default defineNuxtPlugin(async (nuxtApp) => {
+export default defineNuxtPlugin(async () => {
   const site = useSite()
-  const i18n = nuxtApp.$i18n as Composer
-  const { locale } = i18n
 
-  // Initially load the site data
-  if (import.meta.server) {
-    await updateSite()
-  }
+  try {
+    // Response will be cached in payload by default, no need to
+    // handle server/client side differently
+    const data = await $kql(siteQuery)
 
-  // Update the site data on locale change
-  if (import.meta.client) {
-    i18n.onBeforeLanguageSwitch = async (
-      oldLocale: string,
-      newLocale: string,
-    ) => {
-      if (oldLocale !== newLocale) {
-        if (import.meta.dev)
-          console.log('Locale changed:', oldLocale, '->', newLocale)
-
-        await updateSite()
-      }
-    }
-  }
-
-  async function updateSite() {
-    try {
-      const data = await $kql(siteQuery, { language: locale.value })
-
-      site.value = data?.result || {}
-    } catch (e) {
-      console.error('Error loading site data:', (e as FetchError).message)
-    }
+    site.value = data?.result || {}
+  } catch (e) {
+    console.error('Error loading site data:', (e as FetchError).message)
   }
 })
