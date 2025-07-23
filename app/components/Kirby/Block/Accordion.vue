ac944849513c7463701a4d8a7d699c6669444081
snau <kontakt@snau.net>
2025-07-23 17:28:55 +0200

    refactor(Accordion): simplify accordion component structure and update styling for improved visual consistency

--- app/components/Kirby/Block/Accordion.vue
+++ app/components/Kirby/Block/Accordion.vue
@@ -8,29 +8,24 @@ const { block } = defineProps<{
 
 <template>
   <div class="w-full max-w-3xl mx-auto">
-    <UAccordion
-      multiple
-      :ui="{
-        item: '-mx-4 my-2 border-none rounded-xl text-[var(--ui-accordion-color-text)] bg-[var(--ui-accordion-color-closed)] hover:bg-[var(--ui-accordion-color-hover)] data-[state=open]:bg-[var(--ui-accordion-color-open)]',
-        body: 'text-base',
-        trigger: 'text-base font-bold  p-6 cursor-pointer',
-        content: 'px-12 ',
-      }"
-      :items="
-        (
-          (block.content.accordion || []) as Array<{
-            label: string
-            icon?: string
-            content: string
-            disabled?: boolean
-          }>
-        ).map((item) => ({
-          label: item.label,
-          icon: item.icon,
-          content: item.content,
-          disabled: item.disabled || false,
-        }))
-      "
-    />
+    <UAccordion multiple :ui="{
+      item: '-mx-2 my-1 border-none text-[var(--ui-accordion-color-text)] bg-[var(--ui-accordion-color-closed)] hover:bg-[var(--ui-accordion-color-hover)] data-[state=open]:bg-[var(--ui-accordion-color-open)]',
+      body: 'text-base',
+      trigger: 'text-base font-bold  p-6 cursor-pointer',
+      content: 'px-12 ',
+    }" :items="(
+      (block.content.accordion || []) as Array<{
+        label: string
+        icon?: string
+        content: string
+        disabled?: boolean
+      }>
+    ).map((item) => ({
+      label: item.label,
+      icon: item.icon,
+      content: item.content,
+      disabled: item.disabled || false,
+    }))
+      " />
   </div>
 </template>
