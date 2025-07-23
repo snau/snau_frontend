4edb59c09ab14e90a066da4855caf37b4f9ca0d1
snau <kontakt@snau.net>
2025-07-23 17:34:30 +0200

    refactor(Accordion): update item and content padding styles for improved layout consistency

--- app/components/Kirby/Block/Accordion.vue
+++ app/components/Kirby/Block/Accordion.vue
@@ -9,10 +9,10 @@ const { block } = defineProps<{
 <template>
   <div class="w-full max-w-3xl mx-auto">
     <UAccordion multiple :ui="{
-      item: '-mx-2 my-1 border-none text-[var(--ui-accordion-color-text)] bg-[var(--ui-accordion-color-closed)] hover:bg-[var(--ui-accordion-color-hover)] data-[state=open]:bg-[var(--ui-accordion-color-open)]',
+      item: '-mx-2 my-1 border-none text-left text-[var(--ui-accordion-color-text)] bg-[var(--ui-accordion-color-closed)] hover:bg-[var(--ui-accordion-color-hover)] data-[state=open]:bg-[var(--ui-accordion-color-open)]',
       body: 'text-base',
       trigger: 'text-base font-bold  p-6 cursor-pointer',
-      content: 'px-12 ',
+      content: 'px-6 ',
     }" :items="(
       (block.content.accordion || []) as Array<{
         label: string
